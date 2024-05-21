5948f5089cd3da6a21f6a9c1229777d07e23a2e2
Osama <malik.usama5810@gmail.com>
2024-05-22 00:56:55 +0500

    ..

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
-dotenv.config()
+dotenv.config();
 
+const PORT = process.env.PORT || 8000;
 
 connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`⚙️ Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
